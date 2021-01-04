0e0da278cc7892a137632a3dfa71869b0cef4f5c
aydinsenih <aydinsenih@gmail.com>
2021-01-04 12:18:14 -0800

    home page cards added.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import { Layout } from "antd";
+import { Layout, Button, Card } from "antd";
 
 const { Content } = Layout;
 
@@ -7,6 +7,21 @@ const Home = () => {
         <Content>
             <div className="home-top">
                 Get savvy with watering your plants
+                <Button type="primary">JOIN NOW</Button>
+            </div>
+            <div className="cards">
+                <h1>What we offer</h1>
+                <Card title="ORGANIZE">
+                    <p>Keep track of all your plantsin one place, right from the dashboard</p>
+                </Card>
+                <Card>
+                    <h3>FORGET</h3>
+                    <p>We keep track of your watering history and remind you to water,so you don`t have to remember when you last watered or when to water next.</p>
+                </Card>
+                <Card>
+                    <h3>RECORD</h3>
+                    <p>Quickly record watering so you can get back to your other tasks.</p>
+                </Card>
             </div>
         </Content>
     )
